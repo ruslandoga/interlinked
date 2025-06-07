ff9de376ff514e78ad62edd57e917399051aa79e
ruslandoga <ruslandoga+gh@icloud.com>
2025-06-07 19:40:50 +0300

    init

---
+++ lib/interlinked.ex
@@ -0,0 +1,18 @@
+defmodule Interlinked do
+  @moduledoc """
+  Documentation for `Interlinked`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Interlinked.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/interlinked_test.exs
@@ -0,0 +1,8 @@
+defmodule InterlinkedTest do
+  use ExUnit.Case
+  doctest Interlinked
+
+  test "greets the world" do
+    assert Interlinked.hello() == :world
+  end
+end
